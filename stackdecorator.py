82816d3a66b72824224af7ed6502bcef70b7dfe5
Colin Newell <colin.newell@gmail.com>
2013-06-27 06:19:26 +0100

    added logging that allows traces to be extracted

--- stackdecorator.py
+++ stackdecorator.py
@@ -5,8 +5,10 @@ def debugstacktrace(log):
     def inner(func):
 
         def wrapper(*args, **kwargs):
+            log.debug('start stack trace')
             for line in traceback.format_stack():
                 log.debug(line.strip())
+            log.debug('end stack trace')
             retval = func(*args, **kwargs)
             return retval
 
